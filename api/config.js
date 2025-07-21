a1445182e9b8c9ab179a0191fc4e4606f4076fe1
zie2store <kuenastar101@gmail.com>
2025-07-21 12:24:42 +0700

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,6 @@
+export default function handler(request, response) {
+  response.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+  });
+}
